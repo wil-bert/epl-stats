e2f1658f89957cdd427a7eec51b6d1b3142e8b82
wil-bert <ianwilbuts@gmail.com>
2020-10-10 15:16:12 +0300

    add toast

--- app/src/main/java/com/moringaschool/eplstats/MainActivity.java
+++ app/src/main/java/com/moringaschool/eplstats/MainActivity.java
@@ -3,12 +3,24 @@ package com.moringaschool.eplstats;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mFindTeamButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFindTeamButton = (Button)findViewById(R.id.findTeamButton);
+        mFindTeamButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Epl stats", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
