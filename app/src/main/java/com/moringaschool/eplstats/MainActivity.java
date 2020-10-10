2d284aa36855b0f3bd4dc2a712e2fc62e36cf66f
wil-bert <ianwilbuts@gmail.com>
2020-10-11 00:39:19 +0300

    refactore files

--- app/src/main/java/com/moringaschool/eplstats/MainActivity.java
+++ app/src/main/java/com/moringaschool/eplstats/MainActivity.java
@@ -6,21 +6,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import java.lang.reflect.Type;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindTeamButton;
+    private TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mAppNameTextView = findViewById(R.id.appNameTextView);
         mFindTeamButton = (Button)findViewById(R.id.findTeamButton);
         mFindTeamButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, TeamsActivity.class);
+                String team = mFindTeamButton.getText().toString();
+                intent.putExtra("team", team);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/moringaschool/eplstats/TeamsActivity.java
+++ app/src/main/java/com/moringaschool/eplstats/TeamsActivity.java
@@ -2,11 +2,17 @@ package com.moringaschool.eplstats;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class TeamsActivity extends AppCompatActivity {
+    private TextView mTeamListView;
     private String[] teams = new String[] {"Arsenal", "Chelsea", "Tottenham", "Liverpool","Everton", "Leeds", "Aston Villa", "Burnley",
     "New Castle", "Manchester City", "Manchester United"};
     private ListView mListView;
@@ -17,7 +23,23 @@ public class TeamsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_teams);
 
         mListView = (ListView) findViewById(R.id.listView);
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1);
+        mTeamListView = (TextView) findViewById(R.id.teamTextView);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, teams);
         mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String teams = ((TextView)view).getText().toString();
+                Toast.makeText(TeamsActivity.this, teams, Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        Intent intent = getIntent();
+        String teams = intent.getStringExtra("teams");
+        mTeamListView.setText("Teams: " + teams);
+
     }
 }
