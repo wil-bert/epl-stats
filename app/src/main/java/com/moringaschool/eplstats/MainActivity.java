707c30db1b375e110f2a2bc262f71d4d8cad9dda
wil-bert <ianwilbuts@gmail.com>
2020-10-11 21:00:38 +0300

    add onclick to login activity

--- app/src/main/java/com/moringaschool/eplstats/MainActivity.java
+++ app/src/main/java/com/moringaschool/eplstats/MainActivity.java
@@ -14,10 +14,14 @@ import android.widget.Toast;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    @BindView(R.id.findTeamButton)Button mFindTeamButton;
-//    @BindView(R.id.appNameTextView) EditText mAppNameTextView;
-    @BindView(R.id.appNameTextView) TextView mTeamTextView;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.findTeamButton)
+    Button mFindTeamButton;
+    @BindView(R.id.findLoginButton)
+    Button mFindLoginButton;
+    //    @BindView(R.id.appNameTextView) EditText mAppNameTextView;
+    @BindView(R.id.appNameTextView)
+    TextView mTeamTextView;
 //    private static final String TAG = MainActivity.class.getSimpleName();
 //    private Button mFindTeamButton;
 //    private TextView mAppNameTextView;
@@ -30,13 +34,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //        mAppNameTextView = findViewById(R.id.appNameTextView);
 //        mFindTeamButton = (Button)findViewById(R.id.findTeamButton);
         mFindTeamButton.setOnClickListener(this);
+        mFindLoginButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mFindTeamButton) {
+            Intent intent = new Intent(MainActivity.this, TeamsActivity.class);
+            String team = mFindTeamButton.getText().toString();
+            intent.putExtra("team", team);
+            startActivity(intent);
+
+
+        } else if (v == mFindLoginButton) {
+            Intent intent1 = new Intent(MainActivity.this, LoginActivity.class);
+            String login = mFindLoginButton.getText().toString();
+            intent1.putExtra("login", login);
+            startActivity(intent1);
         }
 
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, TeamsActivity.class);
-                String team = mFindTeamButton.getText().toString();
-                intent.putExtra("team", team);
-                startActivity(intent);
-            }
+
     }
+}
