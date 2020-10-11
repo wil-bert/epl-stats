bd86215d532087b1c23d0231c5361097c9224e06
wil-bert <ianwilbuts@gmail.com>
2020-10-11 17:10:46 +0300

    edit custom array adapter

--- app/src/main/java/com/moringaschool/eplstats/MyTeamsArrayAdapter.java
+++ app/src/main/java/com/moringaschool/eplstats/MyTeamsArrayAdapter.java
@@ -1,4 +1,27 @@
 package com.moringaschool.eplstats;
 
-public class MyTeamsArrayAdapter {
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+public class MyTeamsArrayAdapter extends ArrayAdapter {
+    private Context mContext;
+    private String[] mTeams;
+
+    public MyTeamsArrayAdapter(Context mContext, int resources, String[] mTeams) {
+        super(mContext, resources);
+        this.mContext = mContext;
+        this.mTeams = mTeams;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        String teams = mTeams[position];
+        return String.format(teams);
+
+    }
+
+    @Override
+    public int getCount() {
+        return mTeams.length;
+    }
 }
