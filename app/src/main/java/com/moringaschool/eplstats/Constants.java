aa2dc4225c2254145efbdc52d30770a31635e146
wil-bert <ianwilbuts@gmail.com>
2020-10-17 20:48:31 +0300

    refactor constants class

--- app/src/main/java/com/moringaschool/eplstats/Constants.java
+++ app/src/main/java/com/moringaschool/eplstats/Constants.java
@@ -2,8 +2,5 @@ package com.moringaschool.eplstats;
 
 public class Constants {
     public static final String RAPID_BASE_URL = "https://api.rapid.com/v2/";
-    public static final String RAPID_API_KEY = Constants.RAPID_API_KEY;
-
-    private static class RAPID_API_KEY {
-    }
+    public static final String RAPID_API_KEY = BuildConfig.RAPID_API_KEY;
 }
