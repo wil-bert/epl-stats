01456d0f7d05286d5aad45358e2619c1bff41307
wil-bert <ianwilbuts@gmail.com>
2020-10-17 10:58:17 +0300

    add constants

---
+++ app/src/main/java/com/moringaschool/eplstats/Constants.java
@@ -0,0 +1,9 @@
+package com.moringaschool.eplstats;
+
+public class Constants {
+    public static final String RAPID_BASE_URL = "https://api.rapid.com/v2/";
+    public static final String RAPID_API_KEY = Constants.RAPID_API_KEY;
+
+    private static class RAPID_API_KEY {
+    }
+}
