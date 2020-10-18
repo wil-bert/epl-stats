1616dd606ab33895da99f8b42818c843e4d84139
wil-bert <ianwilbuts@gmail.com>
2020-10-18 12:13:26 +0300

    add interface

---
+++ app/src/main/java/com/moringaschool/eplstats/FootballApi.java
@@ -0,0 +1,13 @@
+package com.moringaschool.eplstats;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface FootballApi {
+    @GET("competition/")
+    Call<FootballSearchResponse> getCompetition(
+            @Query("area") String area,
+            @Query("name") String name
+    );
+}
