58de6d5b4b4b28e1e7f214c54432523037d012a3
wil-bert <ianwilbuts@gmail.com>
2020-10-18 11:15:29 +0300

    add base url

--- app/src/main/java/com/moringaschool/eplstats/Constants.java
+++ app/src/main/java/com/moringaschool/eplstats/Constants.java
@@ -1,7 +1,7 @@
 package com.moringaschool.eplstats;
 
 public class Constants {
-    public static final String FOOTBALL_BASE_URL = "https://api.football.com/v2/";
+    public static final String FOOTBALL_BASE_URL = "https://api.football-data.org/v2/";
     public static final String FOOTBALL_API_KEY = BuildConfig.BUILD_TYPE;
 
 }
