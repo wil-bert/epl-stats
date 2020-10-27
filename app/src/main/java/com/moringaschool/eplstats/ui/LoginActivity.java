bd620502c0621e2b770bf2b7ced54842e62a9b92
wil-bert <ianwilbuts@gmail.com>
2020-10-27 22:10:46 +0300

    register account in firebase

--- app/src/main/java/com/moringaschool/eplstats/ui/LoginActivity.java
+++ app/src/main/java/com/moringaschool/eplstats/ui/LoginActivity.java
@@ -2,15 +2,38 @@ package com.moringaschool.eplstats.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.moringaschool.eplstats.R;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.registerTextView)
+    TextView mRegisterTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+        mRegisterTextView.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mRegisterTextView) {
+            Intent intent = new Intent(LoginActivity.this, CreateAcountActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
     }
 }
